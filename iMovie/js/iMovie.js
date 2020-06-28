77175e625dac4570a387076153df0027d300a309
yuanlong.wang <email@yuanlong.wang>
2020-06-28 23:42:51 +0800

    use lock

--- iMovie/js/iMovie.js
+++ iMovie/js/iMovie.js
@@ -16,351 +16,41 @@ function loadscript(url) {
     script.src = url;
     document.body.appendChild(script);
 }
+function checkLockState(lockCallback,unLockCallback){
+	try{
+window.cryptographer = new Jose.WebCryptographer();
+cryptographer.setKeyEncryptionAlgorithm("A128KW");
+cryptographer.setContentEncryptionAlgorithm("A128CBC-HS256");
+window.shared_key = crypto.subtle.importKey("jwk", {"kty":"oct", "k":"XyFAP9qDYielJnUHOK1RtZ"}, {name: "AES-KW"}, true, ["wrapKey", "unwrapKey"]);
+var lockKey=localStorage.getItem('$lock')||"";
+var cookieToken=Cookies.get('MVORZ')||"";
+if(lockKey==""||cookieToken==""){
+	lockCallback();
+	return ;
+}
+var decrypter = new Jose.JoseJWE.Decrypter(window.cryptographer, window.shared_key);
+decrypter.decrypt(cookieToken).then(function(decrypted_plain_text) {
+  if (decrypted_plain_text == lockKey) {
+	unLockCallback();
+  }else{
+	lockCallback();
+  }
+}).catch(function(err) {
+	lockCallback();
+});
+	}catch(e){
+		lockCallback();
+	}
+}
+
 if(mui.os.wechat){
 loadscript("js/clock.js");
 return ;
 }
-var CrossServer="https://jsonp.afeld.me/?url=";
-(function(e){e.retryAjax=function(t){var n;t.tryCount=t.tryCount?t.tryCount:0,t.retryLimit=t.retryLimit?t.retryLimit:2,t.suppressErrors=!0,t.error?(n=t.error,delete t.error):n=function(){},t.complete=function(t,r){if(e.inArray(r,["timeout","abort","error","parsererror"])>-1)return this.tryCount++,this.tryCount<=this.retryLimit?(this.tryCount===this.retryLimit&&(this.error=n,delete this.suppressErrors),e.ajax(this),!0):(window.alert("There was a server error.  Please refresh the page.  If the issue persists, give us a call. Thanks!"),!0)},e.ajax(t)}})(jQuery);
-
-String.prototype.replaceAll = stringReplaceAll;
-
-function stringReplaceAll(AFindText, ARepText) {
-    var raRegExp = new RegExp(AFindText.replace(/([\(\)\[\]\{\}\^\$\+\-\*\?\.\"\'\|\/\\])/g, "\\$1"), "ig");
-    return this.replace(raRegExp, ARepText);
-}
-mui.init({
-    swipeBack: false
+checkLockState(function(){
+loadscript("js/lock.js");
+return;
+},function(){
+loadscript("js/business.js");
 });
-var viewApi = mui('#app').view({
-    defaultPage: '#index'
-});
-var oldBack = mui.back;
-mui.back = function () {
-    if (viewApi.canBack()) { //如果view可以后退，则执行view的后退
-        viewApi.back();
-        jQuery(".player iframe").attr("src", "about:blank");
-		localStorage.removeItem('$currMovie');
-    } else { //执行webview后退
-        oldBack();
-    }
-};
-mui('.mui-scroll-wrapper').scroll({
-    deceleration: 0.0005
-});
-
-var pullRefresh = mui('#offCanvasContentScroll .mui-scroll').pullToRefresh({
-    up: {
-        show: false,
-        contentdown: "",
-        contentnomore: "",
-        contentrefresh: '<div class="moreloading"><span></span><span></span><span></span><span></span><span></span></div>',
-        callback: function () {
-            page = page + 1;
-            if (curType == "search") {
-                searchMovies(jQuery("input[type=search]").val(), page, false);
-            } else {
-                loadMovies(cid, page, false);
-            }
-        }
-    }
-});
-
-var offCanvasWrapper = mui('#offCanvasWrapper');
-var offCanvasInner = offCanvasWrapper[0].querySelector('.mui-inner-wrap');
-var offCanvasSide = document.getElementById("offCanvasSide");
-
-mui('#offCanvasSideScroll').scroll();
-mui('#offCanvasContentScroll').scroll();
-var MovieTMPL = jQuery("#movieTemplate").html();
-var InterTMPL = jQuery("#movieIntrTemplate").html();
-var content = jQuery(".moviecontent ul");
-var MOVIES = {};
-//jQuery.getJSON("https://cors-anywhere.herokuapp.com/https://api.iokzy.com/inc/feifei3s/?m=api&a=json&p=1&g=plus&play=kuyun&cid=5",function(data){
-var cid = 5;
-var page = 1;
-var curType = "list";
-loadMovies(cid, page, true);
-window.showMovies =function (data, clean) {
-    if (clean) {
-        content.empty();
-    }
-    for (var i = 0; i < data.data.length; i++) {
-		
-        var movie = data.data[i];
-		if(movie.vod_cid>19&&movie.vod_cid!=26){continue;}//屏蔽
-        MOVIES[movie.vod_id] = movie;
-        var html = MovieTMPL.replaceAll("{{id}}", movie.vod_id).replaceAll("{{img}}", movie.vod_pic).replaceAll(
-            "{{name}}", movie.vod_name);
-        content.append(html);
-    }
-    jQuery("img.lazy").lazyload({
-        placeholder: "data:image/gif;base64,R0lGODlhCwAPAIAAAFVVVf///yH5BAEAAAEALAAAAAALAA8AAAILhI+py+0Po5y0wgIAOw==",
-        effect: "fadeIn"
-    });
-    hideLoading();
-    if (data.data.length < 30) {
-        pullRefresh.endPullUpToRefresh(true);
-    } else {
-        pullRefresh.endPullUpToRefresh();
-    }
-}
-window.queryProgress=function(id){
-	return localStorage.getItem('$movie_'+id)||"";
-}
-window.setProgress=function(id,num){
-	localStorage.setItem('$movie_'+id,num);
-}
-function loadMovies(cid, pg, clean) {
-    if (clean) {
-        showLoading();
-    }
-    jQuery.retryAjax({
-		url:CrossServer+encodeURIComponent("https://api.iokzy.com/inc/feifei3s/?m=api&a=json&p=" + pg +
-        "&g=plus&play=kuyun&cid=" + cid),
-		timeout: 5000,
-        retryLimit: 3,
-		dataType:"JSON",
-        success:function (data) {
-            showMovies(data, clean);
-			if($(content).height()<window.innerHeight){
-				page=page+1;
-loadMovies(cid,page,false);
-			}
-        }
-		,
-		error:function () {
-		hideLoading();
-        pullRefresh.endPullUpToRefresh();
-    }});
-	setTimeout(function(){
-		hideLoading();
-	},10000);
-}
-
-function searchMovies(wd, pg, clean) {
-    if (wd == "") return;
-    if (clean) {
-        showLoading();
-    }
-     jQuery.retryAjax({
-		 url:CrossServer+encodeURIComponent("https://api.iokzy.com/inc/feifei3s/?m=api&a=json&p=" + pg +
-        "&g=plus&play=kuyun&wd=" + wd),
-        timeout: 5000,
-        retryLimit: 3,
-		dataType:"JSON",
-        success:function (data) {
-            showMovies(data, clean);
-        },
-		error:function () {
-			hideLoading();
-        pullRefresh.endPullUpToRefresh();
-    }});
-}
-
-function showLoading() {
-    jQuery(".mui-mask,.loading").show();
-}
-
-function hideLoading() {
-    jQuery(".loading,.mui-mask").hide();
-}
-var movieinter = jQuery("#movieIntr");
-var PlayLi = '<li class="{{cur}}" data-url="{{url}}"><a>{{name}}</a></li>';
-jQuery(".moviecontent").on("tap", ".mui-card", function () {
-    var id = jQuery(this).data("id");
-    var movie = MOVIES[id];
-    if (movie == undefined) {
-        return;
-    }
-   showMovie(movie);
-   localStorage.setItem('$currMovie',JSON.stringify(movie));
-   saveMovie(movie);
-});
-
-var db;
-
-
-function loadHistory(){
-	showLoading();
-	queryMovie();
-	hideLoading();
-}
-var curPlayID="";
-function showMovie(movie){
-	curPlayID=movie.vod_id;
- var html = InterTMPL.replaceAll("{{name}}", movie.vod_name).replaceAll("{{type}}", movie.list_name).replaceAll(
-        "{{area}}", movie.vod_area).replaceAll(
-        "{{director}}", movie.vod_director).replaceAll(
-        "{{actor}}", movie.vod_actor).replaceAll(
-        "{{content}}", movie.vod_content);
-    movieinter.empty().html(html);
-    if (movie.vod_url != undefined) {
-        var vod_urls = movie.vod_url.split("$$$");
-        if (vod_urls.length > 0) {
-            vod_urls = vod_urls[0].split(/\r\n/);
-            var url = vod_urls[0].split("$")[1];
-			playMovie(url);
-            var html = "";
-			var hisSee="";
-			if(vod_urls.length>1){
-				hisSee=queryProgress(movie.vod_id);
-			}
-            for (var _index = 0; _index < vod_urls.length; _index++) {
-                var _urls = vod_urls[_index].split("$");
-				var cur=_index == 0 ? "current" : "";
-				if(hisSee!=""){
-					if(hisSee!=_urls[0]){
-						cur="";
-					}else{
-						cur= "current";
-						playMovie(_urls[1]);
-					}
-				}
-                html = html + PlayLi.replaceAll("{{cur}}",cur).replaceAll(
-                    "{{url}}", _urls[1]).replaceAll(
-                    "{{name}}", _urls[0]);
-            }
-			
-            jQuery(".playlist ul").empty().html(html);
-        }
-    }
-    jQuery("#moviename").text(movie.vod_name);
-    viewApi.go("#play");
-}
-
-jQuery("#menu").on("tap", "li", function () {
-	var curMenu=jQuery(this);
-    var curCid = curMenu.data("cid");
-    if (curCid!=undefined&&(cid != curCid || curType == "search"||curType=="history")) {
-        cid = curCid||5;
-        page = 1;
-        curType = "list";
-        pullRefresh.refresh(true);
-        jQuery("input[type=search]").val("");
-        loadMovies(cid, page, true);
-    }else{
-		var type=curMenu.data("type");
-		if(type!=undefined&&type=='history'){
-			curType="history";
-			
-			loadHistory();
-		}
-	}
-	document.querySelector("#app .mui-title").innerText=curMenu[0].innerText;
-    offCanvasWrapper.offCanvas('close');
-});
-
-jQuery(".playlist").on("tap", "li", function () {
-    var url = jQuery(this).data("url");
-	playMovie(url);
-    jQuery(".playlist li.current").removeClass("current");
-    jQuery(this).addClass("current");
-	setProgress(curPlayID,jQuery(this).text())
-  });
-
-function playMovie(url){
-	if(url.startsWith("http://")){
-		url=url.replace("http://","https://")
-		}
-    if (url.endsWith(".m3u8")) {
-        url = "https://www.ixxplayer.com/video.php?url=" + url;
-    }
-	jQuery(".player iframe").attr("src", "about:blank");
-    jQuery(".player iframe").attr("src", url || "about:blank");
-}
-
-
-jQuery(".mui-search").on('submit', '.movie-search-form', function (event) {
-    event.preventDefault();
-});
-jQuery("input[type=search]").on("keypress", function (e) {
-    var keycode = e.keyCode;
-    if (keycode == '13') {
-        e.preventDefault();
-        page = 1;
-        curType = "search";
-        pullRefresh.refresh(true);
-        searchMovies(jQuery(this).val(), 1, true);
-    }
-});
-function resize(){
- jQuery(".player,.player iframe").height(parseInt(window.innerWidth*0.5627)>(window.innerHeight-145)?(window.innerHeight-145):parseInt(window.innerWidth*0.5627));
-}
-	resize();
-jQuery(window).resize(function(){
-  resize();
-});
-
-
-if (/(iPhone|iPad|iPod|iOS|Mac)/i.test(navigator.userAgent)) {
-jQuery.getScript("js/jsstore.min.js",function(){
-loadscript("js/history.js");
-});
-}else{
-//设置数据库
-var dbsize = 2 * 2014 * 1024;
-db = openDatabase("iMovie", "", "", dbsize);
-db.transaction(function (tx) {
-	tx.executeSql("CREATE TABLE IF NOT EXISTS movies (id integer PRIMARY KEY,movieData text,last_date datetime)");
-	deleteMovie();
-});
-window.saveMovie=function (mov){
-	 db.transaction(function (tx) {
-		 tx.executeSql("SELECT id FROM movies where id=?", [mov.vod_id],
-                    function (tx, result) {
-                        if (result.rows.length > 0) {
-							tx.executeSql("UPDATE movies set movieData=?,last_date=datetime('now','localtime') where id=?", [JSON.stringify(mov), mov.vod_id],
-						function (tx, result) {}, function (e) {
-						});
-                        }else{
-						 //新增数据
-					tx.executeSql("INSERT INTO movies(id,movieData,last_date) values(?,?,datetime('now','localtime'))", [mov.vod_id, JSON.stringify(mov)],
-						function (tx, result) {}, function (e) {
-						});
-						}
-                    }, function (e) {
-                        
-                    }
-            );
-        });
-}
-window.deleteMovie=function (){
-	db.transaction(function (tx) {
-                    tx.executeSql("DELETE FROM movies WHERE  last_date<datetime('now', '-7 day')", [], function (tx, result) {
-                        
-                    }, function (e) {
-                    });
-    });
-}
-window.queryMovie=function (){
-	db.transaction(function (tx) {
-            //显示list
-            tx.executeSql("SELECT id,movieData,last_date FROM movies ORDER BY last_date desc", [],
-                    function (tx, result) {
-                        if (result.rows.length > 0) {
-							var movies=[];
-                            for (var i = 0; i < result.rows.length; i++) {
-                                item = result.rows.item(i);
-								movies.push(JSON.parse(item["movieData"]));
-                            }
-							var data={"data":movies};
-							showMovies(data,true);
-                        }
-                        
-                    }, function (e) {
-                        
-                    }
-            );
-    });
-}
-}
-
-try{
-	var curMovie=localStorage.getItem('$currMovie') || "";
-	if(curMovie!=""){
-		var mov=JSON.parse(curMovie);
-		showMovie(mov);
-	}
-}catch(e){}
 });
---
+++ iMovie/js/lock.js
@@ -0,0 +1,307 @@
+(function(){jQuery("body").html('<style>*{-webkit-box-sizing:inherit;box-sizing:inherit}body{margin:0;background:#000;text-align:center;font-family:Arial,sans-serif;font-weight:100}svg{width:100%;height:100%}.center{position:absolute;top:50%;left:50%;-ms-transform:translate(-50%,-50%);-webkit-transform:translate(-50%,-50%);transform:translate(-50%,-50%)}.center label{position:relative;margin:0 auto 25px auto;display:inline-block}.padlock{position:relative;width:60px;height:60px;opacity:1;-webkit-transition:.3s ease-in-out;transition:.3s ease-in-out}.padlock.hide{opacity:0}.padlock svg circle{stroke:#a2a2a2}.padlock svg path{fill:#a2a2a2}.padlock.unlock svg circle{stroke:#1fd34a}.padlock.unlock svg path{fill:#1fd34a}.center .input-icon{position:relative;-webkit-transition:.3s ease-in-out;transition:.3s ease-in-out}.center .arrow-icon{position:absolute;top:0;right:0;width:50px;height:100%;background:#fff;display:none;z-index:-2}.center .arrow-icon.show{display:block;z-index:2}.center .arrow-icon svg{fill:#d8d8d8}.center .arrow-icon:hover svg{fill:#444}.center .password{position:relative;width:220px;height:45px;letter-spacing:.05em;line-height:1em;margin:0;padding:2px 70px 2px 15px;outline:0;border:none;border-radius:1px;font-size:14px;-webkit-transition:box-shadow .25s ease;transition:box-shadow .25s ease;-webkit-box-sizing:inherit;box-sizing:inherit}.center .password:focus{-webkit-box-shadow:0 5px 10px rgba(0,0,0,.2);box-shadow:0 5px 10px rgba(0,0,0,.2)}.center .input-icon.unlock{opacity:0}.center .error-message{width:275px;text-align:left;font-size:14px;line-height:1em;background-color:#f0523d;color:#fff;position:absolute;padding:15px;opacity:0;z-index:-1}.center .error-message.show{opacity:1}</style><div class=center><label><div class=padlock><svg id=lock viewBox="0 0 64 64"><circle cx=32 cy=32 fill=none r=31 stroke-width=2></circle><path d="M40.2,29v-4c0-4.5-3.7-8.2-8.2-8.2s-8.2,3.7-8.2,8.2v4H21v15h22V29H40.2z M27.2,25 c0-2.6,2.1-4.8,4.8-4.8s4.8,2.1,4.8,4.8v4h-9.5V25z"></path></svg></div></label><div class=input-icon><input class=password placeholder=自用学习使用，请输入密码 type=password><div class=arrow-icon><svg id=arrow viewBox="0 0 48 44"><polygon points="27.7,13.3 26.3,14.7 31.6,20 14,20 14,22 31.6,22 26.3,27.3 27.7,28.7 35.4,21 "></polygon></svg></div><div class=error-message>密码错误.</div></div></div>');
+
+var hexcase = 0;  /* hex output format. 0 - lowercase; 1 - uppercase        */
+var b64pad  = ""; /* base-64 pad character. "=" for strict RFC compliance   */
+var chrsz   = 8;  /* bits per input character. 8 - ASCII; 16 - Unicode      */
+
+/*
+ * These are the functions you'll usually want to call
+ * They take string arguments and return either hex or base-64 encoded strings
+ */
+function hex_md5(s){ return binl2hex(core_md5(str2binl(s), s.length * chrsz));}
+function b64_md5(s){ return binl2b64(core_md5(str2binl(s), s.length * chrsz));}
+function str_md5(s){ return binl2str(core_md5(str2binl(s), s.length * chrsz));}
+function hex_hmac_md5(key, data) { return binl2hex(core_hmac_md5(key, data)); }
+function b64_hmac_md5(key, data) { return binl2b64(core_hmac_md5(key, data)); }
+function str_hmac_md5(key, data) { return binl2str(core_hmac_md5(key, data)); }
+
+
+/*
+ * Calculate the MD5 of an array of little-endian words, and a bit length
+ */
+function core_md5(x, len)
+{
+  /* append padding */
+  x[len >> 5] |= 0x80 << ((len) % 32);
+  x[(((len + 64) >>> 9) << 4) + 14] = len;
+
+  var a =  1732584193;
+  var b = -271733879;
+  var c = -1732584194;
+  var d =  271733878;
+
+  for(var i = 0; i < x.length; i += 16)
+  {
+    var olda = a;
+    var oldb = b;
+    var oldc = c;
+    var oldd = d;
+
+    a = md5_ff(a, b, c, d, x[i+ 0], 7 , -680876936);
+    d = md5_ff(d, a, b, c, x[i+ 1], 12, -389564586);
+    c = md5_ff(c, d, a, b, x[i+ 2], 17,  606105819);
+    b = md5_ff(b, c, d, a, x[i+ 3], 22, -1044525330);
+    a = md5_ff(a, b, c, d, x[i+ 4], 7 , -176418897);
+    d = md5_ff(d, a, b, c, x[i+ 5], 12,  1200080426);
+    c = md5_ff(c, d, a, b, x[i+ 6], 17, -1473231341);
+    b = md5_ff(b, c, d, a, x[i+ 7], 22, -45705983);
+    a = md5_ff(a, b, c, d, x[i+ 8], 7 ,  1770035416);
+    d = md5_ff(d, a, b, c, x[i+ 9], 12, -1958414417);
+    c = md5_ff(c, d, a, b, x[i+10], 17, -42063);
+    b = md5_ff(b, c, d, a, x[i+11], 22, -1990404162);
+    a = md5_ff(a, b, c, d, x[i+12], 7 ,  1804603682);
+    d = md5_ff(d, a, b, c, x[i+13], 12, -40341101);
+    c = md5_ff(c, d, a, b, x[i+14], 17, -1502002290);
+    b = md5_ff(b, c, d, a, x[i+15], 22,  1236535329);
+
+    a = md5_gg(a, b, c, d, x[i+ 1], 5 , -165796510);
+    d = md5_gg(d, a, b, c, x[i+ 6], 9 , -1069501632);
+    c = md5_gg(c, d, a, b, x[i+11], 14,  643717713);
+    b = md5_gg(b, c, d, a, x[i+ 0], 20, -373897302);
+    a = md5_gg(a, b, c, d, x[i+ 5], 5 , -701558691);
+    d = md5_gg(d, a, b, c, x[i+10], 9 ,  38016083);
+    c = md5_gg(c, d, a, b, x[i+15], 14, -660478335);
+    b = md5_gg(b, c, d, a, x[i+ 4], 20, -405537848);
+    a = md5_gg(a, b, c, d, x[i+ 9], 5 ,  568446438);
+    d = md5_gg(d, a, b, c, x[i+14], 9 , -1019803690);
+    c = md5_gg(c, d, a, b, x[i+ 3], 14, -187363961);
+    b = md5_gg(b, c, d, a, x[i+ 8], 20,  1163531501);
+    a = md5_gg(a, b, c, d, x[i+13], 5 , -1444681467);
+    d = md5_gg(d, a, b, c, x[i+ 2], 9 , -51403784);
+    c = md5_gg(c, d, a, b, x[i+ 7], 14,  1735328473);
+    b = md5_gg(b, c, d, a, x[i+12], 20, -1926607734);
+
+    a = md5_hh(a, b, c, d, x[i+ 5], 4 , -378558);
+    d = md5_hh(d, a, b, c, x[i+ 8], 11, -2022574463);
+    c = md5_hh(c, d, a, b, x[i+11], 16,  1839030562);
+    b = md5_hh(b, c, d, a, x[i+14], 23, -35309556);
+    a = md5_hh(a, b, c, d, x[i+ 1], 4 , -1530992060);
+    d = md5_hh(d, a, b, c, x[i+ 4], 11,  1272893353);
+    c = md5_hh(c, d, a, b, x[i+ 7], 16, -155497632);
+    b = md5_hh(b, c, d, a, x[i+10], 23, -1094730640);
+    a = md5_hh(a, b, c, d, x[i+13], 4 ,  681279174);
+    d = md5_hh(d, a, b, c, x[i+ 0], 11, -358537222);
+    c = md5_hh(c, d, a, b, x[i+ 3], 16, -722521979);
+    b = md5_hh(b, c, d, a, x[i+ 6], 23,  76029189);
+    a = md5_hh(a, b, c, d, x[i+ 9], 4 , -640364487);
+    d = md5_hh(d, a, b, c, x[i+12], 11, -421815835);
+    c = md5_hh(c, d, a, b, x[i+15], 16,  530742520);
+    b = md5_hh(b, c, d, a, x[i+ 2], 23, -995338651);
+
+    a = md5_ii(a, b, c, d, x[i+ 0], 6 , -198630844);
+    d = md5_ii(d, a, b, c, x[i+ 7], 10,  1126891415);
+    c = md5_ii(c, d, a, b, x[i+14], 15, -1416354905);
+    b = md5_ii(b, c, d, a, x[i+ 5], 21, -57434055);
+    a = md5_ii(a, b, c, d, x[i+12], 6 ,  1700485571);
+    d = md5_ii(d, a, b, c, x[i+ 3], 10, -1894986606);
+    c = md5_ii(c, d, a, b, x[i+10], 15, -1051523);
+    b = md5_ii(b, c, d, a, x[i+ 1], 21, -2054922799);
+    a = md5_ii(a, b, c, d, x[i+ 8], 6 ,  1873313359);
+    d = md5_ii(d, a, b, c, x[i+15], 10, -30611744);
+    c = md5_ii(c, d, a, b, x[i+ 6], 15, -1560198380);
+    b = md5_ii(b, c, d, a, x[i+13], 21,  1309151649);
+    a = md5_ii(a, b, c, d, x[i+ 4], 6 , -145523070);
+    d = md5_ii(d, a, b, c, x[i+11], 10, -1120210379);
+    c = md5_ii(c, d, a, b, x[i+ 2], 15,  718787259);
+    b = md5_ii(b, c, d, a, x[i+ 9], 21, -343485551);
+
+    a = safe_add(a, olda);
+    b = safe_add(b, oldb);
+    c = safe_add(c, oldc);
+    d = safe_add(d, oldd);
+  }
+  return Array(a, b, c, d);
+
+}
+
+/*
+ * These functions implement the four basic operations the algorithm uses.
+ */
+function md5_cmn(q, a, b, x, s, t)
+{
+  return safe_add(bit_rol(safe_add(safe_add(a, q), safe_add(x, t)), s),b);
+}
+function md5_ff(a, b, c, d, x, s, t)
+{
+  return md5_cmn((b & c) | ((~b) & d), a, b, x, s, t);
+}
+function md5_gg(a, b, c, d, x, s, t)
+{
+  return md5_cmn((b & d) | (c & (~d)), a, b, x, s, t);
+}
+function md5_hh(a, b, c, d, x, s, t)
+{
+  return md5_cmn(b ^ c ^ d, a, b, x, s, t);
+}
+function md5_ii(a, b, c, d, x, s, t)
+{
+  return md5_cmn(c ^ (b | (~d)), a, b, x, s, t);
+}
+
+/*
+ * Calculate the HMAC-MD5, of a key and some data
+ */
+function core_hmac_md5(key, data)
+{
+  var bkey = str2binl(key);
+  if(bkey.length > 16) bkey = core_md5(bkey, key.length * chrsz);
+
+  var ipad = Array(16), opad = Array(16);
+  for(var i = 0; i < 16; i++)
+  {
+    ipad[i] = bkey[i] ^ 0x36363636;
+    opad[i] = bkey[i] ^ 0x5C5C5C5C;
+  }
+
+  var hash = core_md5(ipad.concat(str2binl(data)), 512 + data.length * chrsz);
+  return core_md5(opad.concat(hash), 512 + 128);
+}
+
+/*
+ * Add integers, wrapping at 2^32. This uses 16-bit operations internally
+ * to work around bugs in some JS interpreters.
+ */
+function safe_add(x, y)
+{
+  var lsw = (x & 0xFFFF) + (y & 0xFFFF);
+  var msw = (x >> 16) + (y >> 16) + (lsw >> 16);
+  return (msw << 16) | (lsw & 0xFFFF);
+}
+
+/*
+ * Bitwise rotate a 32-bit number to the left.
+ */
+function bit_rol(num, cnt)
+{
+  return (num << cnt) | (num >>> (32 - cnt));
+}
+
+/*
+ * Convert a string to an array of little-endian words
+ * If chrsz is ASCII, characters >255 have their hi-byte silently ignored.
+ */
+function str2binl(str)
+{
+  var bin = Array();
+  var mask = (1 << chrsz) - 1;
+  for(var i = 0; i < str.length * chrsz; i += chrsz)
+    bin[i>>5] |= (str.charCodeAt(i / chrsz) & mask) << (i%32);
+  return bin;
+}
+
+/*
+ * Convert an array of little-endian words to a string
+ */
+function binl2str(bin)
+{
+  var str = "";
+  var mask = (1 << chrsz) - 1;
+  for(var i = 0; i < bin.length * 32; i += chrsz)
+    str += String.fromCharCode((bin[i>>5] >>> (i % 32)) & mask);
+  return str;
+}
+
+/*
+ * Convert an array of little-endian words to a hex string.
+ */
+function binl2hex(binarray)
+{
+  var hex_tab = hexcase ? "0123456789ABCDEF" : "0123456789abcdef";
+  var str = "";
+  for(var i = 0; i < binarray.length * 4; i++)
+  {
+    str += hex_tab.charAt((binarray[i>>2] >> ((i%4)*8+4)) & 0xF) +
+           hex_tab.charAt((binarray[i>>2] >> ((i%4)*8  )) & 0xF);
+  }
+  return str;
+}
+
+/*
+ * Convert an array of little-endian words to a base-64 string
+ */
+function binl2b64(binarray)
+{
+  var tab = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  var str = "";
+  for(var i = 0; i < binarray.length * 4; i += 3)
+  {
+    var triplet = (((binarray[i   >> 2] >> 8 * ( i   %4)) & 0xFF) << 16)
+                | (((binarray[i+1 >> 2] >> 8 * ((i+1)%4)) & 0xFF) << 8 )
+                |  ((binarray[i+2 >> 2] >> 8 * ((i+2)%4)) & 0xFF);
+    for(var j = 0; j < 4; j++)
+    {
+      if(i * 8 + j * 6 > binarray.length * 32) str += b64pad;
+      else str += tab.charAt((triplet >> 6*(3-j)) & 0x3F);
+    }
+  }
+  return str;
+}
+var password = document.querySelector('.password');
+var error = document.querySelector('.error-message');
+var padlock = document.querySelector('.padlock');
+var arrow = document.querySelector('.arrow-icon');
+var input = document.querySelector('.input-icon');
+var pass = "wCFzumWbU6XRQjB3As9Nww";
+var rand = "KRtTqAyicMP1ZV5rGBIdDU0szofYS3xm"
+password.addEventListener('keypress', function(e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) {
+        if (password.value != "") {
+            if (b64_md5(password.value+rand) != pass) {
+                error.classList.add('show');
+                padlock.classList.remove('unlock');
+            } else {
+                error.classList.remove('show');
+                unlock();
+            }
+        }
+    }
+});
+password.addEventListener('keyup', function() {
+    if (password.value == "") {
+        error.classList.remove('show');
+        padlock.classList.remove('unlock');
+        arrow.classList.remove('show');
+    } else {
+        arrow.classList.add('show');
+    }
+});
+arrow.addEventListener('click', function() {
+    if (b64_md5(password.value+rand) != pass) {
+        error.classList.add('show');
+        padlock.classList.remove('unlock');
+    } else {
+        error.classList.remove('show');
+        unlock();
+    }
+}, false);
+function randPass(len){
+for (var e =len, a = "abcdefghijklmnopqrstuvwxyz", t = "ABCDEFGHIJKLMNOPQRSTUVWXYZ", r = "0123456789",  n = "", o = n; n.length < e; )
+            entity1 = Math.ceil(a.length * Math.random() * Math.random()),
+                entity2 = Math.ceil(r.length * Math.random() * Math.random()),
+                entity4 = Math.ceil(t.length * Math.random() * Math.random()),
+             n.length < e && (n += a.charAt(entity1)),
+            n.length < e && (n += t.charAt(entity4)),
+             n.length < e && (n += r.charAt(entity2)),
+            o == n && (rand = Math.floor(4 * Math.random()) + 1,
+                1 == rand ? n += a.charAt(entity1) : 2 == rand ? n += t.charAt(entity4) : 3 == rand ? n += r.charAt(entity2) : 4 == rand && (n += i.charAt(entity3)),
+                o = n);
+return n;
+}
+function unlock() {
+    padlock.classList.add('unlock');
+    input.classList.add('unlock');
+var randKey=randPass(32);
+localStorage.setItem('$lock',randKey)
+	var encrypter = new Jose.JoseJWE.Encrypter(window.cryptographer, window.shared_key);
+encrypter.encrypt(randKey).then(function(result) {
+	Cookies.set('MVORZ',result,{ expires: 30 });
+}).catch(function(err) {});
+    setTimeout(function() {
+        padlock.classList.add('hide');
+        setTimeout(function() {
+            window.location.href = '';
+        }, 900);
+    }, 600);
+}
+})();
