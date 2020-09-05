fee1c6e1cd1b6fbd0810143618111371649be872
yuanlong.wang <email@yuanlong.wang>
2020-09-05 11:17:11 +0800

    update

--- iMovie/js/business.js
+++ iMovie/js/business.js
@@ -1,6 +1,6 @@
 (function(){
 var CrossServer="https://jsonp.afeld.me/?url=";
-(function(e){e.retryAjax=function(t){var n;t.tryCount=t.tryCount?t.tryCount:0,t.retryLimit=t.retryLimit?t.retryLimit:2,t.suppressErrors=!0,t.error?(n=t.error,delete t.error):n=function(){},t.complete=function(t,r){if(e.inArray(r,["timeout","abort","error","parsererror"])>-1)return this.tryCount++,this.tryCount<=this.retryLimit?(this.tryCount===this.retryLimit&&(this.error=n,delete this.suppressErrors),e.ajax(this),!0):(window.alert("There was a server error.  Please refresh the page.  If the issue persists, give us a call. Thanks!"),!0)},e.ajax(t)}})(jQuery);
+(function(e){e.retryAjax=function(t){var n;t.tryCount=t.tryCount?t.tryCount:0,t.retryLimit=t.retryLimit?t.retryLimit:2,t.suppressErrors=!0,t.error?(n=t.error,delete t.error):n=function(){},t.complete=function(t,r){if(e.inArray(r,["timeout","abort","error","parsererror"])>-1)return this.tryCount++,this.tryCount<=this.retryLimit?(this.tryCount===this.retryLimit&&(this.error=n,delete this.suppressErrors),e.ajax(this),!0):(console.log("There was a server error.  Please refresh the page.  If the issue persists, give us a call. Thanks!"),!0)},e.ajax(t)}})(jQuery);
 
 String.prototype.replaceAll = stringReplaceAll;
 
@@ -100,7 +100,7 @@ function loadMovies(cid, pg, clean) {
 		url:CrossServer+encodeURIComponent("https://api.iokzy.com/inc/feifei3s/?m=api&a=json&p=" + pg +
         "&g=plus&play=kuyun&cid=" + cid),
 		timeout: 5000,
-        retryLimit: 3,
+        retryLimit: 10,
 		dataType:"JSON",
         success:function (data) {
             showMovies(data, clean);
@@ -128,7 +128,7 @@ function searchMovies(wd, pg, clean) {
 		 url:CrossServer+encodeURIComponent("https://api.iokzy.com/inc/feifei3s/?m=api&a=json&p=" + pg +
         "&g=plus&play=kuyun&wd=" + wd),
         timeout: 5000,
-        retryLimit: 3,
+        retryLimit: 10,
 		dataType:"JSON",
         success:function (data) {
             showMovies(data, clean);
@@ -225,7 +225,7 @@ function refreshMoives(vodid,vodname){
 		 url:CrossServer+encodeURIComponent("https://api.iokzy.com/inc/feifei3s/?m=api&a=json&p=" + 1 +
         "&g=plus&play=kuyun&wd=" + vodname),
         timeout: 5000,
-        retryLimit: 3,
+        retryLimit: 10,
 		dataType:"JSON",
         success:function (data) {
            for (var i = 0; i < data.data.length; i++) {
